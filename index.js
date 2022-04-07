03b5c245a198c6068354710378c8966264778916
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-07 13:49:06 +0530

    basic logic js

---
+++ index.js
@@ -0,0 +1,28 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var checkBtn = document.querySelector("#check-btn")
+var output = document.querySelector("#output");
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        // loss logic
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;    
+    } 
+    else if (initial < current) {
+        // profit logic
+        var profit = (current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100;
+    } 
+    else {
+
+    }
+}
+
+function clickHandler() {
+    
+}
+
+checkBtn.addEventListener('click', clickHandler);
+
