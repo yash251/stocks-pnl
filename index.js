34e9f4515395a7c6ebdf2adb09daab4fd736e45d
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-07 14:33:21 +0530

    limiting decimal to 2

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
         // loss logic
         var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100; 
+        var lossPercentage = ((loss / initial) * 100).toFixed(2); 
         output.innerText = (`loss is ${loss} which is ${lossPercentage}% of initial`);
     } 
     else if (initial < current) {
         // profit logic
         var profit = (current - initial) * quantity;
-        var profitPercentage = (profit / initial) * 100;
+        var profitPercentage = ((profit / initial) * 100).toFixed(2);
         output.innerText = (`profit is ${profit} which is ${profitPercentage}% of initial`);
     } 
     else {
