79d819cfe5fb290695c0bbcae9f682af24becb8f
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-07 15:09:59 +0530

    raw js , html

--- index.js
+++ index.js
@@ -3,32 +3,48 @@ var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var checkBtn = document.querySelector("#check-btn")
 var output = document.querySelector("#output");
+var main = document.querySelector("#main");
 
-function calculateProfitAndLoss(initial, quantity, current) {
-    if (initial > current) {
-        // loss logic
-        var loss = (initial - current) * quantity;
-        var lossPercentage = ((loss / initial) * 100).toFixed(2); 
-        output.innerText = (`loss is ${loss} which is ${lossPercentage}% of initial`);
-    } 
-    else if (initial < current) {
-        // profit logic
-        var profit = (current - initial) * quantity;
-        var profitPercentage = ((profit / initial) * 100).toFixed(2);
-        output.innerText = (`profit is ${profit} which is ${profitPercentage}% of initial`);
-    } 
-    else {
-        output.innerText = (`no profit no loss`);
+checkBtn.addEventListener('click', () => {
+    clickHandler(initialPrice.value, stocksQuantity.value, currentPrice.value, output);
+  });
+  
+  function clickHandler(initial, quantity, current, op) {
+  
+    if (initial !== "" && quantity !== "" && current !== "") {
+      if (initial >= 0 && quantity >= 0 && current >= 0) {
+        initial = parseFloat(initial);
+        quantity = parseFloat(quantity);
+        current = parseFloat(current);
+        if (initial > current) {
+            // loss logic
+            var loss = (initial - current) * quantity;
+            var lossPercentage = ((loss / initial) * 100).toFixed(2); 
+          
+            output.textContent = `Aww you just lost ₹${loss} which is ${lossPercentage}% of the initial price.`;
+            if (parseFloat(lossPercentage) >= 50) {
+                main.style.backgroundColor = "red";
+          }
+        } 
+        else if (initial < current) {
+            // profit logic
+            var profit = (current - initial) * quantity;
+            var profitPercentage = ((profit / initial) * 100).toFixed(2);
+
+            output.textContent = `Yayy! You just earned profit of ₹${profit} which is ${profitPercentage}% of the initial price.`;
+            if (parseFloat(profitPercentage) >= 50) {
+                main.style.backgroundColor = "green";
+          }
+        } else {
+          console.log("No profit no loss");
+          output.textContent = "No profit no loss";
+        }
+      } else {
+        console.log("Positive Amount Only");
+        output.textContent = "Positive Amount Only";
+      }
+    } else {
+      console.log("Please enter all details");
+      output.textContent = "Please enter all details";
     }
-}
-
-function clickHandler() {
-    var ip = initialPrice.value;
-    var qty = stocksQuantity.value;
-    var curr = currentPrice.value;
-
-    calculateProfitAndLoss(ip, qty, curr)
-}
-
-checkBtn.addEventListener('click', clickHandler);
-
+  }
