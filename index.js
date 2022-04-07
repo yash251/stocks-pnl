afb79f5e52b4a604e97b249c1a6685d75cd73636
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-07 13:59:25 +0530

    consoling the values

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
         // loss logic
         var loss = (initial - current) * quantity;
-        var lossPercentage = (loss / initial) * 100;    
+        var lossPercentage = (loss / initial) * 100; 
+        console.log(`loss is ${loss} which is ${lossPercentage}% of initial`);
     } 
     else if (initial < current) {
         // profit logic
         var profit = (current - initial) * quantity;
         var profitPercentage = (profit / initial) * 100;
+        console.log(`profit is ${profit} which is ${profitPercentage}% of initial`);
     } 
     else {
-
+        console.log(`no profit no loss`);
     }
 }
 
@@ -25,7 +27,7 @@ function clickHandler() {
     var qty = stocksQuantity.value;
     var curr = currentPrice.value;
 
-    calculateProfitAndLoss(ip, qty, curr);
+    calculateProfitAndLoss(ip, qty, curr)
 }
 
 checkBtn.addEventListener('click', clickHandler);
